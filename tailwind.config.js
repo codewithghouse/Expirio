5da7155239443712e3524c105f914e2ec0c13f9c
codewithghouse <personalghouse@gmail.com>
2025-12-24 15:55:24 +0530

    fix m layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          50: '#ecfdf5',
+          100: '#d1fae5',
+          200: '#a7f3d0',
+          300: '#6ee7b7',
+          400: '#34d399',
+          500: '#10b981',
+          600: '#059669',
+          700: '#047857',
+          800: '#065f46',
+          900: '#064e3b',
+          950: '#022c22',
+        },
+        sage: {
+          50: '#f7fbf9',
+          100: '#eef6f2',
+          200: '#dcece5',
+          300: '#c0dad0',
+          400: '#9dbfb4',
+          500: '#7da197',
+          600: '#62837a',
+          700: '#516a63',
+          800: '#445652',
+          900: '#3a4845',
+        }
+      },
+      borderRadius: {
+        'xl': '1rem',
+        '2xl': '1.5rem',
+        '3xl': '2rem',
+      }
+    },
   },
   plugins: [],
 }
